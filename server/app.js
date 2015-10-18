16f45fafc2e5397d5240da15f90c371beef58163
vardhman1996 <vardhman.mehta96@gmail.com>
2015-10-18 10:40:57 +0000

    Get users

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize("postgres://username:asdfasdf@localhost:5432/lift");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -23,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/me', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,9 +33,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://postgres:asdfasdf@localhost:5432/lift');
+var User = require('../models/User');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var newId = 5; //req.user.id
+	User.findAll({
+		where: {
+			id: newId
+		}
+        }).then(function(user) {
+		res.send(user);
+	})
 });
 
+router.route('/create')
+	.get(function(req,res,next) {
+		res.send("Hello! You are creating something");
+	})
+	.post(function(req,res,next) {
+
+	});
 module.exports = router;

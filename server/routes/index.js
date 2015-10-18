77a1b91de3677b9facbfc376f3714d02ea3f1a3f
Justin Harjanto <justinharjanto@hotmail.com>
2015-10-18 01:18:58 -0700

    Test basic FB passport integration

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,48 @@ var express = require('express');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var router = express.Router();
+var FACEBOOK_APP_ID = "957421080985378";
+var FACEBOOK_APP_SECRET = "0d53e9e2387f0d3462c53cf6b3556016";
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+            clientID: FACEBOOK_APP_ID,
+            clientSecret: FACEBOOK_APP_SECRET,
+            callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      function(accessToken, refreshToken, profile, done) {
+          // asynchronous verification, for effect...
+          process.nextTick(function () {
+              // To keep the example simple, the user's Facebook profile is returned to
+              // represent the logged-in user.  In a typical application, you would want
+              // to associate the Facebook account with a user record in your database,
+              // and return that user instead.
+              return done(null, profile);
+          });
+      }
+));
+
+
+router.get('/auth/facebook', passport.authenticate('facebook', { failureRedirect : '/login'}),
+        function(req, res) {
+            res.redirect('/')
+        });
+
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
 
 
